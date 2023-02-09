2d89585e5d0367554338f8a5879758335c399aee
Keiwan Jamaly <keiwan@jamaly.de>
2023-02-09 09:50:29 +0100

    something is working :)

--- Flow.py
+++ Flow.py
@@ -1,10 +1,12 @@
 import numpy as np
 import numpy.typing as npt
-from scipy.integrate import solve_ivp
+from scipy.integrate import solve_ivp, trapezoid
 import matplotlib.pyplot as plt
 from numba import njit
-import sys
-import pickle
+import pandas as pd
+import h5py
+import os
+import argparse
 
 
 @njit
@@ -50,7 +52,7 @@ class Flow():
         k = self.k(t)
         beta = self.__beta
         N = self.__N_Flavor
-        return - k**3 / (2*np.pi*self.e_b(k, ux)*N) * (1+self.n_b(beta * self.e_b(k, ux)))
+        return - k**3 / (2*np.pi*self.e_b(k, ux)*N) * (1+2*self.n_b(beta * self.e_b(k, ux)))
 
     def f(self, t, u):
         self.__print_counter += 1
@@ -66,11 +68,6 @@ class Flow():
     def compute(self):
         self.__solution = solve_ivp(
             self.f, [0, self.t(self.__kir)], self.__u_init, lband=1, uband=1, method="LSODA", rtol=1e-10, atol=1e-10)
-        print(self.__solution)
-        pickle.dump(self.__solution, open('test.pkl', 'wb'))
-        print(sys.getsizeof(self.__solution.y))
-
-        return self.__solution
 
     def k(self, t):
         return self.__Lambda * np.exp(-t)
@@ -93,13 +90,107 @@ class Flow():
     def get_grid(self):
         return self.__grid
 
+    def get_observables_at_pos(self, j):
+        t = self.__solution.t[j]
+        current_root = 0
+        current_root_value = 0
+        y = self.__solution.y[:, j]
+        grid = self.__grid
+        massSquare = (y[1] - y[0])/(grid[1] - grid[0])
+
+        for i in range(1, len(y)-1):
+            # find bracketed root
+            if y[i] < 0 and y[i+1] > 0:
+                # linear interpolate the bracketed interval and find the root
+                test_root = grid[i] - y[i] * \
+                    (grid[i+1] - grid[i])/(y[i+1] - y[i])
+
+                # calculate the potential at the minimum
+                rest_potential_interval = test_root * \
+                    y[i] - grid[i] * y[i] + \
+                    ((test_root-grid[i])**2*(y[i+1] - y[i])) / \
+                    (2*(grid[i+1] - grid[i]))
+                testPotential = trapezoid(
+                    y[:i+1], grid[:i+1]) + rest_potential_interval
+                if current_root_value > testPotential:
+                    current_root_value = testPotential
+                    current_root = test_root
+                    massSquare = (y[i+1] - y[i])/(grid[i+1] - grid[i])
+
+        return {"sigma": current_root, "massSquare": massSquare, "pressure": - current_root_value, "t": t, "k": self.k(t), "y": y}
+
+    def get_observables_for_all_positions(self):
+        observable_array = [self.get_observables_at_pos(
+            i) for i in range(len(self.__solution.t))]
+        return pd.DataFrame(observable_array)
+
+    def save(self, path):
+        # create folder, if it doesn't exist
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        # generate filenames
+        filename = f'mu={self.__mu}_T={self.__T}_sigmaMax={self.__grid[-1]}_Lambda={self.__Lambda}_kir={self.__kir}_nGrid={len(self.__grid)}_nFlavor={self.__N_Flavor}.hdf5'
+        path_and_filename = os.path.join(path, filename)
+
+        # compute observables
+        observables = self.get_observables_for_all_positions()
+
+        with h5py.File(path_and_filename, "w") as f:
+            f.attrs["sigmaMax"] = self.__grid[-1]
+            f.attrs["NFlavor"] = self.__N_Flavor
+            f.attrs["Lambda"] = self.__Lambda
+            f.attrs["kir"] = self.__kir
+            f.attrs["NGrid"] = len(self.__grid)
+            f.attrs["mu"] = self.__mu
+            f.attrs["T"] = self.__T
+            f.create_dataset("t", data=observables["t"])
+            f.create_dataset("sigma", data=observables["sigma"])
+            f.create_dataset("massSquare", data=observables["massSquare"])
+            f.create_dataset("pressure", data=observables["pressure"])
+            f.create_dataset("k", data=observables["k"])
+            f.create_dataset("grid", data=self.__grid)
+            y_dset = f.create_dataset("y", (observables["y"].to_numpy().shape[0], len(
+                self.__grid)))
+
+            for i, elem in enumerate(observables["y"].to_numpy()):
+                y_dset[i, :] = elem[:]
+
 
 def main():
-    simple_heat = Flow(1e5, 1e-4, 6, 1000, 0.1, 0.01, 2)
-    solution = simple_heat.compute()
-    grid = simple_heat.get_grid()
-    # plt.plot(grid, solution.y[:, -1])
-    # plt.show()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-N", type=int,
+                        help="flavor number")
+    parser.add_argument("-L", type=float,
+                        help="ultra-violet cutoff")
+    parser.add_argument("-kir", type=float,
+                        help="infrared cutoff")
+    parser.add_argument("-sigmaMax", type=float,
+                        help="maximum of spatial computation domain")
+    parser.add_argument("-grid", type=int,
+                        help="number of grid points")
+    parser.add_argument("-mu", type=float,
+                        help="chemical potential")
+    parser.add_argument("-T", type=float,
+                        help="Temperature")
+    parser.add_argument("-o", type=str,
+                        help="output file path")
+    args = parser.parse_args()
+    Lambda = args.L
+    kir = args.kir
+    n_flavor = args.N
+    sigma_max = args.sigmaMax
+    mu = args.mu
+    T = args.T
+    n_grid = args.grid
+
+    path = args.o
+
+    flow = Flow(Lambda, kir, sigma_max, n_grid, mu, T, n_flavor)
+    # flow = Flow(1e5, 1e-4, 6, 1000, 0.4, 0.0125, 2)
+
+    flow.compute()
+    flow.save(path)
 
 
 if __name__ == "__main__":
---
+++ sigmaMax_test_T=0.0125_mu=0.6.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+Lambda=100000
+Kir=0.0001
+T=0.0125
+mu=0.6
+N=2
+deltaSigma=0.006
+
+for ((i=2;i<=26;i++)); do
+    z=`echo "var=$i.;var/=2;var" | bc -l`
+    grid=`echo "var=$z;var/=$deltaSigma;var" | bc`
+    python3 Flow.py -N $N -L $Lambda -kir $Kir -sigmaMax $z -grid $grid -mu $mu -T $T -o spatial_domain_test/T=${T}_mu=$mu
+done
