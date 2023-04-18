20941f7b591ffd12f24cf46973c0bb715aa34fb9
Keiwan Jamaly <keiwanjamaly@gmail.com>
2023-04-18 10:41:33 +0200

    some changes

--- src/grid.c
+++ src/grid.c
@@ -1,46 +1,64 @@
 #include "grid.h"
-#include <stdlib.h>
 #include <stdio.h>
 
-struct grid *initialize_grid(int N, double *points)
+Grid *create_grid(int N, double *points)
 {
-    struct grid *new_grid = malloc(sizeof(struct grid));
+    Grid *new_grid = malloc(sizeof(Grid));
     new_grid->N = N - 2;
     new_grid->grid_points = malloc(new_grid->N * sizeof(double));
+
     double *grid_midpoints = malloc((new_grid->N + 1) * sizeof(double));
     new_grid->dx = malloc((new_grid->N + 1) * sizeof(double));
     new_grid->dx_midpoints = malloc(new_grid->N * sizeof(double));
 
-    // copy grid points over
-    new_grid->grid_left = points[0];
-    for (int i = 1; i < (N - 1); i++)
-    {
-        new_grid->grid_points[i - 1] = points[i];
-    }
-    new_grid->grid_right = points[N - 1];
-
-    // fill dx and grid_midpoints array
-    new_grid->dx[0] = new_grid->grid_points[0] - new_grid->grid_left;
-    grid_midpoints[0] = (new_grid->grid_points[0] + new_grid->grid_left) / 2;
-    for (int i = 1; i < new_grid->N; i++)
-    {
-        new_grid->dx[i] = new_grid->grid_points[i] - new_grid->grid_points[i - 1];
-        grid_midpoints[i] = (new_grid->grid_points[i] + new_grid->grid_points[i - 1]) / 2;
-    }
-    new_grid->dx[new_grid->N] = new_grid->grid_right - new_grid->grid_points[new_grid->N - 1];
-    grid_midpoints[new_grid->N] = (new_grid->grid_right + new_grid->grid_points[new_grid->N - 1]) / 2;
-
-    // fill dx_midpoints_array
-    for (int i = 0; i < new_grid->N; i++)
-    {
-        new_grid->dx_midpoints[i] = grid_midpoints[i + 1] - grid_midpoints[i];
-    }
+    copy_internal_grid_points(new_grid, N, points);
+    compute_dx(new_grid, N, points);
+    compute_grid_midpoints(new_grid, N, points, grid_midpoints);
+    compute_dx_midpoints(new_grid, N, grid_midpoints);
 
     free(grid_midpoints);
     return new_grid;
 }
 
-void free_grid(struct grid *grid_to_be_freed)
+void copy_internal_grid_points(Grid *grid, int N, double *points)
+{
+    grid->grid_left = points[0];
+    for (int i = 1; i < (N - 1); i++)
+    {
+        grid->grid_points[i - 1] = points[i];
+    }
+    grid->grid_right = points[N - 1];
+}
+
+void compute_dx(Grid *grid, int N, double *points)
+{
+    grid->dx[0] = grid->grid_points[0] - grid->grid_left;
+    for (int i = 1; i < grid->N; i++)
+    {
+        grid->dx[i] = grid->grid_points[i] - grid->grid_points[i - 1];
+    }
+    grid->dx[grid->N] = grid->grid_right - grid->grid_points[grid->N - 1];
+}
+
+void compute_grid_midpoints(Grid *grid, int N, double *points, double *grid_midpoints)
+{
+    grid_midpoints[0] = (grid->grid_points[0] + grid->grid_left) / 2;
+    for (int i = 1; i < grid->N; i++)
+    {
+        grid_midpoints[i] = (grid->grid_points[i] + grid->grid_points[i - 1]) / 2;
+    }
+    grid_midpoints[grid->N] = (grid->grid_right + grid->grid_points[grid->N - 1]) / 2;
+}
+
+void compute_dx_midpoints(Grid *grid, int N, double *grid_midpoints)
+{
+    for (int i = 0; i < grid->N; i++)
+    {
+        grid->dx_midpoints[i] = grid_midpoints[i + 1] - grid_midpoints[i];
+    }
+}
+
+void destroy_grid(Grid *grid_to_be_freed)
 {
     free(grid_to_be_freed->grid_points);
     free(grid_to_be_freed->dx);
--- src/grid.h
+++ src/grid.h
@@ -1,14 +1,23 @@
 #ifndef GRID_H
 #define GRID_H
 
-struct grid
+#include <stdlib.h>
+
+typedef struct grid
 {
     int N;
+    double grid_left;
+    double grid_right;
     double *grid_points;
     double *dx;
     double *dx_midpoints;
-    double grid_left;
-    double grid_right;
-};
+} Grid;
 
-#endif
+Grid *create_grid(int N, double *points);
+void copy_internal_grid_points(Grid *grid, int N, double *points);
+void compute_dx(Grid *grid, int N, double *points);
+void compute_grid_midpoints(Grid *grid, int N, double *points, double *grid_midpoints);
+void compute_dx_midpoints(Grid *grid, int N, double *grid_midpoints);
+void destroy_grid(Grid *grid_to_be_freed);
+
+#endif // GRID_H
