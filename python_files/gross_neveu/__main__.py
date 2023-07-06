85b06602ddee435d9bd66ac8bae7f6e7c14ea931
Keiwan Jamaly <keiwan@jamaly.de>
2023-07-06 15:17:06 +0200

    added some comfortability features with non-evenly spaced grid cells

--- python_files/gross_neveu/__main__.py
+++ python_files/gross_neveu/__main__.py
@@ -1,5 +1,5 @@
 from python_files.gross_neveu.Gross_Neveu import GN_2p1, GN_1p1
-from python_files.grid_creator import create_homogenious_grid
+from python_files.grid_creator import create_homogenious_grid, create_inhomogenious_grid_from_number_of_cells
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
@@ -36,7 +36,14 @@ def main():
     mu = args.mu
     T = args.T
     N_Flavor = args.N if args.N is not None else np.inf
-    grid_points = create_homogenious_grid(sigma_max, N_Grid)
+    if N_Grid > 1000:
+        print(
+            f'Number of gridpoints is {N_Grid}, this is larger than 1000 so using unevenly spaced grid points instead')
+        grid_points = create_inhomogenious_grid_from_number_of_cells(
+            sigma_max, N_Grid)
+    else:
+        grid_points = create_homogenious_grid(sigma_max, N_Grid)
+
     if args.d == 1:
         sol = GN_1p1(grid_points, Lambda, kir,
                      samples, mu, T, N_Flavor=N_Flavor)
@@ -55,7 +62,6 @@ def main():
     plt.plot(x, y)
     plt.xlabel(r'$\sigma$')
     plt.ylabel(r'u')
-    # plt.xlim([0.0, 1.2])
     plt.grid()
     plt.show()
 
