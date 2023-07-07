5ce5d1ad2a1c39ac26088d7a6200da7d1f084223
Keiwan Jamaly <keiwan@jamaly.de>
2023-07-07 18:47:21 +0200

    restore of lost commit with a map instead of apply_async

--- python_files/phase_diagram/__main__.py
+++ python_files/phase_diagram/__main__.py
@@ -4,22 +4,26 @@ import h5py
 import argparse
 from python_files.gross_neveu.couplings.couplings_io import get_exact_coupling_from_file
 from python_files.gross_neveu.couplings.couplings_io import generate_filename
-from python_files.phase_diagram.computation_function import compute_simga
+from python_files.phase_diagram.computation_function import compute_sigma_spread
 
 
 def main():
     mu_max = 1.2
     T_max = 1.0
-    mu_array = np.arange(0, mu_max, 0.02)
-    T_array = np.arange(0.01, T_max, 0.02)
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '-N', type=int, help='Set the number of flavors', required=True, default=None)
     parser.add_argument('-L', type=float,
                         help='Set the UV Cutoff', required=True, default=None)
+    parser.add_argument('--delta', type=float,
+                        help='sets the resolution for the phase diagram', default=0.02)
 
     args = parser.parse_args()
+    phase_diagram_resolution = args.delta
+    mu_array = np.arange(0, mu_max, phase_diagram_resolution)
+    T_array = np.arange(0.01, T_max, phase_diagram_resolution)
+
     if args.N == -1:
         N_Flavor = np.inf
     else:
@@ -41,25 +45,21 @@ def main():
     h = 1.0
     sigma_0 = 1.0
 
+    mu_T_list = []
     job_list = []
 
     for mu in mu_array:
         for T in T_array:
-            job_list.append([mu, T])
+            mu_T_list.append([mu, T])
+            job_list.append([one_over_g2, dimension, mu, T, sigma_max,
+                             Lambda, kir, delta_sigma, N_Flavor, h, sigma_0])
 
     with Pool() as p:
-        result_async = [p.apply_async(compute_simga, (one_over_g2, dimension, mu, T, sigma_max,
-                                                      Lambda, kir, delta_sigma, N_Flavor, h, sigma_0)) for (mu, T) in job_list]
-        result = [async_obj.get() for async_obj in result_async]
-
-    combined_result = []
-    for i in range(len(job_list)):
-        mu, T = job_list[i]
-        combined_result.append([mu, T, result[i]])
+        result = p.map(compute_sigma_spread, job_list)
 
     with h5py.File(f'./data/phase_diagram/phase_diagram_Lambda_{Lambda}_N_Flavor_{N_Flavor}.hdf5', "w") as f:
         f.attrs["coupling"] = one_over_g2
-        f.create_dataset("phase_diagram", data=combined_result)
+        f.create_dataset("phase_diagram", data=result)
 
 
 if __name__ == "__main__":
