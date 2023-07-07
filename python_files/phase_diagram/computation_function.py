cdc23141ccf1eb2dac91f82e51ee8c6554729b77
Keiwan Jamaly <keiwan@jamaly.de>
2023-07-07 18:19:07 +0200

    changed to pool map, since this is a little more concise)

--- python_files/phase_diagram/computation_function.py
+++ python_files/phase_diagram/computation_function.py
@@ -1,13 +1,18 @@
 from python_files.gross_neveu.compute_observable import sigma
 
 
-def compute_simga(one_over_g2, dimension, mu, T, sigma_max,
+def compute_sigma_spread(x):
+    return compute_sigma(*x)
+
+
+def compute_sigma(one_over_g2, dimension, mu, T, sigma_max,
                   Lambda, kir, delta_sigma, N_Flavor, h, sigma_0):
     try:
         result = sigma(one_over_g2, dimension, mu, T, sigma_max,
                        Lambda, kir, delta_sigma, N_Flavor, h, sigma_0)
-        print(f'T = {T}, mu = {mu} - done')
+        print(f'mu = {mu}, T = {T}, result = {result} - done')
         return result
     except:
-        print(f'T = {T}, mu = {mu} - error')
-        return -1
+        result = -1
+        print(f'mu = {mu}, T = {T} result = {result}- error')
+        return result
